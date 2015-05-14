bf7e4a95eefd3fdc436fb95abbf336ceca497adf
Grzegorz Białek <grzegorz@bialek.it>
2015-05-14 17:36:19 +0200

    car consumption list

--- sowieso/src/main/java/pl/chiqvito/sowieso/ui/model/CarConsumptionModel.java
+++ sowieso/src/main/java/pl/chiqvito/sowieso/ui/model/CarConsumptionModel.java
@@ -1,6 +1,7 @@
 package pl.chiqvito.sowieso.ui.model;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,18 +41,37 @@ public class CarConsumptionModel extends BaseModel {
         private TextView txtName;
         private TextView txtDate;
         private TextView txtCombustion;
+        private TextView txtDistance;
+        private TextView txtPetrolStation;
+        private TextView txtPrice;
+        private TextView txtRefuelAmount;
+        private TextView txtTotalCost;
 
         private ViewHolder(View itemView) {
             super(itemView);
             txtCombustion = (TextView) itemView.findViewById(R.id.txt_car_cons_combustion);
             txtDate = (TextView) itemView.findViewById(R.id.txt_car_cons_date);
             txtName = (TextView) itemView.findViewById(R.id.txt_car_cons_name);
+            txtDistance = (TextView) itemView.findViewById(R.id.txt_car_cons_distance);
+            txtPetrolStation = (TextView) itemView.findViewById(R.id.txt_car_cons_petrol_station);
+            txtPrice = (TextView) itemView.findViewById(R.id.txt_car_cons_price);
+            txtRefuelAmount = (TextView) itemView.findViewById(R.id.txt_car_cons_refuel_amount);
+            txtTotalCost = (TextView) itemView.findViewById(R.id.txt_car_cons_total_cost);
         }
 
         public void bindView(InventoryCarConsumptionDTO dto) {
-            txtName.setText(dto.getCar().getName());
-            txtCombustion.setText(dto.getCombustion().toString());
-            txtDate.setText(dto.getRefuelDateString());
+            try {
+                txtName.setText(dto.getCar().getName());
+                txtCombustion.setText(dto.getCombustion().toString() + " l/km");
+                txtDate.setText(dto.getRefuelDateString());
+                txtDistance.setText(dto.getDistance().toString() + " km");
+                txtPetrolStation.setText(dto.getPetrolStation().name());
+                txtPrice.setText(dto.getPrice().toString() + " " + dto.getCurrency().name());
+                txtRefuelAmount.setText(dto.getRefuelAmount().toString() + " l");
+                txtTotalCost.setText(dto.getTotalCost().toString() + " " + dto.getCurrency().name());
+            } catch (Exception e) {
+                Log.e(CarConsumptionModel.class.getName(), e.getMessage(), e);
+            }
         }
     }
 
